3d236c8e32e015ea57fa2096940251f0664a2692
Ubuntu dskp <valdy@ciencias.unam.mx>
2017-11-27 21:28:14 -0800

    With Foreign keys

--- example.sql
+++ example.sql
@@ -1,5 +1,7 @@
--- CREATING TABLES --
+--FOREIGN KEYS USE --
+PRAGMA foreign_keys = ON;
 
+-- CREATING TABLES --
 CREATE TABLE country(
 	id INTEGER PRIMARY KEY ASC, name TEXT);
 
@@ -16,3 +18,10 @@ CREATE TABLE morra_country(id INTEGER PRIMARY KEY ASC,
 	id_country INTEGER, id_morra INTEGER);
 
 -- create one to one relation morra_instagram --
+CREATE TABLE morra_instagram(
+	id INTEGER PRIMARY KEY ASC, morra_id INTEGER, instagram_id INTEGER
+	 FOREIGN KEY(morra_id) REFERENCES morra(id),
+	 FOREIGN KEY(instagram_id) REFERENCES instagram(id));
+
+--CREATING THE DATABASE--
+INSERT INTO country (id,name) VALUES (1, 'Mexico');
