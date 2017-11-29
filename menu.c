d68c7cb48b2d27c0dec77422b28a4de417f01735
larachicharo <laralacesar@gmail.com>
2017-11-28 21:23:05 -0600

    Creating interactive menu.
    Showing 5 most followed accounts ready.

--- menu.c
+++ menu.c
@@ -5,7 +5,7 @@
 #define DBNAME "ex.sqlt"
 
 
-int callback(void *data, int argc, char **argv, char **azColName){
+int select_callback(void *data, int argc, char **argv, char **azColName){
 	int i;
 	for (i = 0; i<argc; i++){
 		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
@@ -22,21 +22,40 @@ void handle_rc(int rc, sqlite3 **ppDb) {
 	}
 }
 
+void print_menu(void) {
+	puts("Press: q to quit");
+	puts("a to show the 5 most followed instagram accounts");
+	puts("x to x.");
+}
+
 int main(void) {
 	sqlite3 *ppDb;
 	int rc;
+	char *errmsg;
 	
 	rc = sqlite3_open(
 		DBNAME,
 		&ppDb);
 
 	handle_rc(rc, &ppDb);
-
-	char *errmsg;
-	char *q = "SELECT * FROM instagram ORDER BY nfollowers DESC LIMIT 1;";
-	rc = sqlite3_exec(ppDb, q, callback, NULL, &errmsg);
 	
-	handle_rc(rc, &ppDb);
+	
+	char c;
+	char *q;
+	print_menu();
+	while((c = getchar()) != 'q') {
+		if (c == '\n')
+			continue;
+		if (c == 'a')
+			q = "SELECT * FROM instagram ORDER BY nfollowers DESC LIMIT 5;";
+		
+		rc = sqlite3_exec(ppDb, q, select_callback, NULL, &errmsg);
+		handle_rc(rc, &ppDb);
+		
+		print_menu();
+	}
+
+	
 	sqlite3_close(ppDb);
 	return 0;
 }
